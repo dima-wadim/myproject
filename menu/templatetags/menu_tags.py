66e67c6ec93ddd235201e6604ece81b1f132fc96
Вадим <kantora_70@mail.ru>
2024-07-25 10:38:24 +0500

    Изменение добавление

--- menu/templatetags/menu_tags.py
+++ menu/templatetags/menu_tags.py
@@ -1,26 +1,9 @@
-import re
 from django import template
-from django.urls import reverse
-from menu.models import Menu, MenuItem
+from menu.models import MenuItem
 
 register = template.Library()
 
 @register.inclusion_tag('menu/menu.html', takes_context=True)
 def draw_menu(context, menu_name):
-    request = context['request']
-    current_url = request.path
-    menu = Menu.objects.get(name=menu_name)
-    items = menu.items.all()
-    return {
-        'menu': menu,
-        'items': items,
-        'current_url': current_url,
-    }
-
-def parse_url(url):
-    if re.match(r'^https?://', url):
-        return url
-    try:
-        return reverse(url)
-    except:
-        return url
+    menu_items = MenuItem.objects.filter(parent__isnull=True)
+    return {'menu_items': menu_items, 'current_path': context['request'].path}
